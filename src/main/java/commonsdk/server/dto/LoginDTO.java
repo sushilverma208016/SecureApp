b27ca899d972b1f174a73adfb47d3d4366e7f8c1
akash <agarwalakash@vmware.com>
2017-11-24 02:06:24 +0530

    dto

---
+++ src/main/java/commonsdk/server/dto/LoginDTO.java
@@ -0,0 +1,14 @@
+package commonsdk.server.dto;
+
+public class LoginDTO {
+    String username;
+    String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
